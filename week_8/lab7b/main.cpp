b47a707e2bdbf1bae0656d5057a16d21a954d9d6
AthertonH <athertonharrison@gmail.com>
2022-10-26 19:33:16 -0700

    unlv

--- week_8/lab7b/main.cpp
+++ week_8/lab7b/main.cpp
@@ -1,14 +1,21 @@
 /*
     Name: Harrison Atherton, 5005662748, 1019, #7B
-    Description:
-    Input:
-    Output:
+    Description: Program to look through a text file, organize it into an array,
+    and use that array to determine the lowest temperature, highest temperature, average
+    low temperature, and average high temperature
+    Input: User will be prompted to enter in a file name. If the file cannot be found, 
+    the user will be asked again.
+    Output: The city and year will be displayed, with calculations of the lowest
+    temperature, highest temperature, average low temperature, and average high 
+    temperature.
 */
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
+// Declare constants for array
 int const MONTHS = 12;
 int const TEMPS = 2;
 
@@ -21,11 +28,15 @@ int main()
     string city = "";
     int year = 0;
     double arr[MONTHS][TEMPS];
+    double lowestTempYear = 100;
+    double highestTempYear = 0;
+    double lowTempSum = 0;
+    double highTempSum = 0;
 
     // Open the file
     do
     {
-        cout << "Enter a file name\n**";
+        cout << "Enter file name\n**";
         cin >> fileName;
         iFile.open(fileName);
 
@@ -35,6 +46,7 @@ int main()
         }
     } while(!iFile.is_open());
 
+    // Grab the city as a string, and the year as an integer
     getline(iFile, city, ' ');
     iFile >> year;
 
@@ -51,13 +63,34 @@ int main()
     }
     // Close the file
     iFile.close();
-
-
+    
     // Sort the array
-    
-    
+    for(int i = 0; i < MONTHS; i++)
+    {
+        for(int j = 0; j < TEMPS; j++)
+        {
+            // Determine if lowest temp is lowest overall
+            if(arr[i][0] < lowestTempYear)
+            {
+                lowestTempYear = arr[i][j];
+            }
+            // Determine if highest temp is highest overall
+            if(arr[i][1] > highestTempYear)
+            {
+                highestTempYear = arr[i][j];
+            }
+        }
+        // Add lowest and highest temp to seperate sums
+        lowTempSum += arr[i][0];
+        highTempSum += arr[i][1];
+    }
 
-    cout << city << endl << year;
+    // Output results to terminal
+    cout << fixed << setprecision(2) << endl << city << " " << year << endl
+         << "Lowest temperature of the year was " << lowestTempYear << "° F." << endl
+         << "Highest temperature of the year was " << highestTempYear << "° F." << endl
+         << "Average low temperature of the year was " << lowTempSum/MONTHS << "° F." << endl
+         << "Average high temperature of the year was " << highTempSum/MONTHS << "° F." << endl;
     
-
+    return 0;
 }
