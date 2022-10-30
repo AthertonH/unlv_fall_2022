41d81f1c242b701f0dd7d57d678544b96c3266cd
AthertonH <athertonharrison@gmail.com>
2022-10-29 18:00:30 -0700

    unlv

---
+++ personal/code_wars/growth_of_a_population.cpp
@@ -0,0 +1,19 @@
+/*
+Growth of a Population
+https://www.codewars.com/kata/563b662a59afc2b5120000c6
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Arge
+{
+  public:
+  static int nbYear(int p0, double percent, int aug, int p);
+};
+
+int main()
+{
+    
+}
