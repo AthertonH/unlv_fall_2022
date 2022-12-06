b8b47873343f776108e3579a40f1ae975910af6b
AthertonH <athertonharrison@gmail.com>
2022-12-05 22:41:42 -0800

    unlv

---
+++ personal/code_wars/7_kyu/beginner_series_#3_sum_of_numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*
+Given two integers a and b, which can be positive or negative, find the sum 
+of all the integers between and including them and return it. If the two numbers 
+are equal return a or b.
+Note: a and b are not ordered!
+*/
+
+// int get_sum(int a, int b)
+// {
+//   for(int i = a; i <= b; i++)
+//         a += i;
+//     return a;
+// }
+
+int get_sum(int a, int b)
+{
+    int x = a + b;
+    for(int i = a; x <= )
+}
+
+int main()
+{
+    cout << get_sum(10, 20);
+    return 0;
+}
