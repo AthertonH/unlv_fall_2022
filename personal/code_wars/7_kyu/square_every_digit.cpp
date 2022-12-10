127f47081a680a4a7146375de2d2c30a129ade81
AthertonH <athertonharrison@gmail.com>
2022-12-09 21:45:35 -0800

    unlv

---
+++ personal/code_wars/7_kyu/square_every_digit.cpp
@@ -0,0 +1,28 @@
+/*
+https://www.codewars.com/kata/546e2562b03326a88e000020
+Welcome. In this kata, you are asked to square every digit of a number 
+and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, 
+because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int square_digits(int num) 
+{
+    
+    
+}
+
+int main()
+{
+    
+
+    return 0;
+}
