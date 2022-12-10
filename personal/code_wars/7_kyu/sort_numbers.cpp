997a5cc614b85a846e2f268274344e937f685b1c
AthertonH <athertonharrison@gmail.com>
2022-12-09 21:45:38 -0800

    unlv

---
+++ personal/code_wars/7_kyu/sort_numbers.cpp
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5174a4c0f2769dd8b1000003
+// Finish the solution so that it sorts the passed in array of numbers. 
+// If the function passes in an empty array or null/nil value then 
+// it should return an empty array.
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> nums) 
+{
+    int temp = nums[0]; 
+    
+}
