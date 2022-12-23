a213ff76f711f08d285613a29ae1f34a882cce9c
AthertonH <athertonharrison@gmail.com>
2022-12-22 21:12:23 -0800

    unlv

--- personal/code_wars/7_kyu/find_the_stray_number.cpp
+++ personal/code_wars/7_kyu/find_the_stray_number.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 // You are given an odd-length array of integers, in which all of them are the same, 
 // except for one single number.
 
@@ -9,22 +5,37 @@ using namespace std;
 // different number.
 
 // The input array will always be valid! (odd-length >= 3)
+#include <iostream>
+#include <vector>
+using namespace std;
 
+/*
+My solution didn't work
 int stray(vector<int> numbers) 
 {
+    int strayNumber = numbers[0];
     for(unsigned int i = 0; i < numbers.size(); i++)
     {
-        if(i == i+1)
-            continue;
-        else
-            return i;
+        if(strayNumber != numbers[i])
+            return strayNumber = numbers[i];
     }
+    return 1;
+}
+*/
+
+// This is the solution I found on codewars
+int stray(vector<int> numbers) 
+{
+    int res = 0;
+    for(int n: numbers) res ^= n;
+    return res;
 }
 
 int main()
 {
-    vector<int> = {1, 1, 1,}
+    vector<int> test = {1, 1, 1, 4, 1, 1, 1};
 
+    cout << stray(test);
 
     return 0;
 }
